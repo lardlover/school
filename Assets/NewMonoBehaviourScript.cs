7dcac635dade2f4d906a608f2bff4c7cb5b6650b
lardlover <aaroncoyoc@gmail.com>
2026-09-04 13:23:56 -0400

    update

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -1,16 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI textbox;
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonClickMethod()
     {
-        
+        textbox.text = "I have changed";
     }
 }
